7fc4441ebcefea4abc3d5f63e64d41ee26e6dcf9
MatthewShurtz <59268254+MatthewShurtz@users.noreply.github.com>
2020-09-24 16:03:17 -0500

    Added JS

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-const user_input = document.getElementById("");
+const addButton = document.getElementById("add-button");
+
+addButton.addEventListener("click", addTask);
+addButton.addEventListener("click", clearField);
+
+function clearField() {
+  document.getElementById("text-box").value = "";
+}
+
+function addTask() {
+  let newItem = document.createElement("li");
+  let userInput = document.getElementById("text-box").value;
+  let newAddedTask = document.createTextNode(userInput);
+  newItem.appendChild(newAddedTask);
+  let taskList = document.getElementById("task-list");
+  taskList.appendChild(newItem);
+}
